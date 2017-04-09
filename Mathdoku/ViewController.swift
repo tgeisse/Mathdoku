d2c4113d3cd9eed9324e886eb42d100e01c658a3
Taylor Geisse <taylorgeisse@Taylors-MacBook-Pro-3.local>
2017-04-09 13:29:58 -0700

    Initial Commit

---
+++ Mathdoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mathdoku
+//
+//  Created by Taylor Geisse on 4/9/17.
+//  Copyright © 2017 Taylor Geisse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

7f73dac27cd5601fd51a3a917f20b919be4d6b50
Taylor Geisse <tgeisse@andrew.cmu.edu>
2017-11-14 22:56:50 -0800

    Working on settings menu Moved more folders Added Defaults library

---
+++ Mathdoku/MenuViewControllers/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  Mathdoku
+//
+//  Created by Taylor Geisse on 11/14/17.
+//  Copyright © 2017 Taylor Geisse. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}

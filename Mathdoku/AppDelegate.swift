1f80a20f99a1b96561c2166e859a6ae0b2e5379b
Taylor Geisse <tgeisse@andrew.cmu.edu>
2018-05-29 21:41:30 -0700

    removing the blur overlay. Cheaters are going to cheat - this is just a nuisance

--- Mathdoku/AppDelegate.swift
+++ Mathdoku/AppDelegate.swift
@@ -114,6 +114,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         
+        /* removing the blur effect - cheaters are going to cheat. This is just a nuisance
         DebugUtil.print("app losing active focus")
         
         let blurEffect = UIBlurEffect(style: .dark)
@@ -124,6 +125,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.addSubview(blurEffectView)
             
         DebugUtil.print("blur overlay should have been added")
+        */
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -138,15 +140,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         DebugUtil.print("returning to app")
-        /*
-        if let blurEffectView = self.window?.viewWithTag(221122) {
-            UIView.animate(withDuration: 0.5, animations: {
-                blurEffectView.alpha = 0.0
-            }, completion: { finished in
-                blurEffectView.removeFromSuperview()
-            })
-        }*/
-        self.window?.viewWithTag(221122)?.removeFromSuperview()
+        // self.window?.viewWithTag(221122)?.removeFromSuperview()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {

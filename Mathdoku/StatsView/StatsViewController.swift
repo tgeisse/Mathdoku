d931ca2fa41c134471ac7864aaa9554f98e83b26
Taylor Geisse <1383010+tgeisse@users.noreply.github.com>
2020-04-07 22:11:38 -0700

    created view controller for the Stats View

---
+++ Mathdoku/StatsView/StatsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StatsViewController.swift
+//  Mathdoku
+//
+//  Created by Taylor Geisse on 4/7/20.
+//  Copyright © 2020 Taylor Geisse. All rights reserved.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
